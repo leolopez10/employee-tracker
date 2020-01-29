db36ed5dac926c3a8bac03cd1f8938be97a522ba
lil L <57925562+leolopez10@users.noreply.github.com>
2020-01-29 12:41:37 -0600

    dislplayed all employee info

--- employeeDB.sql
+++ employeeDB.sql
@@ -3,15 +3,11 @@ CREATE database employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE employee (
+
+CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
+  department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY(id)
-  -- FOREIGN KEY(role_id) REFERENCES employee_role(id),
-  -- FOREIGN KEY(manager_id) REFERENCES employee_role(id)
 );
 
 CREATE TABLE employee_role (
@@ -19,14 +15,19 @@ CREATE TABLE employee_role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  PRIMARY KEY(id)
-  -- FOREIGN KEY(department_id) REFERENCES department(id)
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(role_id) REFERENCES employee_role(id),
+  FOREIGN KEY(manager_id) REFERENCES employee_role(id)
 );
 
 
@@ -38,55 +39,60 @@ VALUES ("operations");
 INSERT INTO department (department_name)
 VALUES ("marketing");
 
-INSERT INTO department (department_name)
-VALUES ("coaching");
-
-INSERT INTO department (department_name)
-VALUES ("playing");
-
 -- creating employee-roles
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES ("Director", 125000.00, 1);
+VALUES ("COO", 125000.00, 1);
 
 INSERT INTO employee_role (title, salary, department_id)
 VALUES ("Manager", 85000.00, 1);
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES ("Manager", 85000.00, 2);
-
-INSERT INTO employee_role (title, salary, department_id)
-VALUES ("Manager", 85000.00, 3);
-
-INSERT INTO employee_role (title, salary, department_id)
 VALUES ("employee", 50000.00, 1);
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES ("employee", 50000.00, 2);
+VALUES ("PR Lead", 85000.00, 2);
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES ("employee", 50000.00, 3);
+VALUES ("PR Associate", 50000.00, 2);
+
 
 -- creating employees
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Leo", "Lopez", 1);
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("Leo", "Lopez", 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Blake", "Patterson", 2);
+VALUES ("Blake", "Patterson", 5);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Rodrigo", "Liques", 3);
+VALUES ("Rodrigo", "Liques", 2);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Alberto", "Rodriguez", 4);
+VALUES ("Alberto", "Rodriguez", 3);
+
+
+-- reads all of the employee list
+
+SELECT 
+	employee.id,
+	employee.first_name, 
+	employee.last_name, 
+	employee_role.title, 
+	department.department_name, 
+	employee_role.salary
+FROM 
+	employee
+INNER JOIN 
+	employee_role ON employee.role_id=employee_role.id OR employee.manager_id=employee_role.id
+INNER JOIN 
+	department ON employee_role.department_id=department.id
+ORDER BY
+	employee.id
+ASC;
 
 
 
-
-
-
-
-SELECT * FROM employee;
 select * from department;
 select * from employee_role;
+SELECT * FROM employee;
--- employeeTracker.js
+++ employeeTracker.js
@@ -13,5 +13,69 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log(connection.threadId);
+    // runEmployeeTracker();
+    getAllEmployees();
 });
+
+
+function openingQuestions() {
+    inquirer.
+    prompt([{
+        type: "rawlist",
+        message: "Navigate Employee DataBase \n What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all employees by department",
+            "View all employes by manager",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update employee information",
+            "Remove employee from database",
+        ]
+
+    }])
+}
+
+async function runEmployeeTracker() {
+    try {
+
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+function getAllEmployees() {
+    var query =
+        `SELECT 
+        employee.id,
+        employee.first_name, 
+        employee.last_name, 
+        employee_role.title, 
+        department.department_name, 
+        employee_role.salary
+    FROM 
+        employee
+    INNER JOIN 
+        employee_role ON employee.role_id=employee_role.id OR employee.manager_id=employee_role.id
+    INNER JOIN 
+        department ON employee_role.department_id=department.id
+    ORDER BY
+        employee.id
+    ASC;`
+
+    connection.query(query, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            let employeeData = {
+                id: res[i].id,
+                first_name: res[i].first_name,
+                last_name: res[i].last_name,
+                title: res[i].title,
+                department_name: res[i].department_name,
+                salary: res[i].salary
+            }
+            console.table([employeeData]);
+        }
+    })
+}
